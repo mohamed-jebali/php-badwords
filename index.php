49c3ccd96d746ca0d2de8bffb0aae3943ef35e5f
mohamed-jebali <mohamedjebali992010@gmail.com>
2023-06-30 16:15:24 +0200

    correction method

--- index.php
+++ index.php
@@ -5,13 +5,19 @@
         <link rel="stylesheet" href="https://cdn.jsdelivr.net/npm/bootstrap@5.3.0/dist/css/bootstrap.min.css">
     </head>
 <body>
-    <form class='p-5' action="./server.php" method='GET'>
-        <input class='mb-4' type="text"name='name'> <br>
-        <input class='mb-4' type="text"name='censored'> <br>
+    <div class="container text-center p-5">
+        <div class="row">
+        <div class="col-12">
+        <form class='' action="./server.php" method='POST'>
+        <input class='mb-4 p-2' type="text"name='name' placeholder='Inserisci il nome'> <br>
+        <input class='mb-4 p-2' type="text"name='censored' placeholder='Inserisci la password'> <br>
         <button class='btn btn-primary'>
             Invia
         </button>
     </form>
+            </div>
+        </div>
+    </div>
 </body>
 </html>
 
--- server.php
+++ server.php
@@ -1,13 +1,32 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Response Php Badwords</title>
+    <link rel="stylesheet" href="https://cdn.jsdelivr.net/npm/bootstrap@5.3.0/dist/css/bootstrap.min.css">
+</head>
+<body>
+    
+</body>
+</html>
+
 <?php
-    $name = $_GET['name'];
-    $censored = $_GET['censored'];
+    $name = $_POST['name'];
+    $censored = $_POST['censored'];
     $lengthString = 'la lunghezza è : ';
 ?>
 
-<div>
+<div class='container p-5'>
+    <div class="row">
+        <div class="col-12 text-center">
             <?php echo $name;?> <br>
             <?php echo $lengthString . strlen($name); ?> <br>
+            <hr>
             <!-- ------------- -->
             <?php echo str_replace($censored,'***',$censored)?> <br>
             <?php echo $lengthString . strlen($censored); ?>
+        </div>
+    </div>
 </div>
