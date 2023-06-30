2784d31c81cdb1863a306f088ddd906d9fe563d9
mohamed-jebali <mohamedjebali992010@gmail.com>
2023-06-30 15:27:16 +0200

    added string lenght

--- server.php
+++ server.php
@@ -1,9 +1,11 @@
 <?php
     $name = $_GET['name'];
     $censored = $_GET['censored'];
+    $lengthString = 'la lunghezza del paragrafo è : ';
 ?>
 
 <div>
-            <?php echo $name; ?> <br>
-            <?php echo $censored; ?>
+            <?php echo $name;?> <br>
+            <?php echo $censored; ?> <br>
+            <?php echo $lengthString . strlen($name); ?>
 </div>
